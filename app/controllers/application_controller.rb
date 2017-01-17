4131a810a361021bf5367dc10e8bab0012960b15
Marat Nasyrov <marat.online@live.ru>
2017-01-18 01:01:36 +0300

    add auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Authentication
   include Authorization
   
+  before_action :authenticate_user!
 
   protect_from_forgery with: :exception
 
